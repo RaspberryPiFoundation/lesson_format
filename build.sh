fe3967926b77b27e3321a9ed5326e5ca614d4df9
tef <tef@twentygototen.org>
2013-12-09 16:11:59 +0000

    Cleanup of directory structure, splitting out things nicely

--- build.sh
+++ build.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
+MARKDOWN="markdown_github+header_attributes+yaml_metadata_block+inline_code_attributes"
+PANDOC_HTML="pandoc -f $MARKDOWN -t html5 -s --highlight-style pygments -c codeclub.css --section-divs --template=templates/template.html"
+#PANDOC_PDF="pandoc -f $MARKDOWN -t latex  --latex-engine=pdflatex"
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OUT=output
-pushd $DIR > /dev/null
-rm -fR $OUT
-mkdir -p $OUT
-touch $OUT/.keep
+OUT=output; pushd $DIR > /dev/null
+rm -fR $OUT; mkdir -p $OUT; touch $OUT/.keep
 
-
-legal="templates/UK_LEGAL.md"
-
-MARKDOWN="markdown_github+header_attributes+yaml_metadata_block+inline_code_attributes"
-
-PANDOC_HTML="pandoc -f $MARKDOWN -t html5 -s --highlight-style pygments -c codeclub.css --section-divs --template=templates/template.html"
-
-PANDOC_PDF="pandoc -f $MARKDOWN -t latex  --latex-engine=pdflatex"
-
+UK_LEGAL="./templates/uk_legal.html"
+WORLD_LEGAL="./templates/world_legal.html"
+LEGAL="$WORLD_LEGAL"
 
 for f in examples/example.md;
 do
 	base=`basename "$f"`
 	output="lesson${base%%.*}.html"
-	$PANDOC_HTML --include-after-body "$legal" "$f" -o "$OUT/$output"
+	$PANDOC_HTML --include-after-body "$LEGAL" "$f" -o "$OUT/$output"
 	#pdf="lesson${base%%.*}.pdf"
-	#cat "$f" "$legal" | $PANDOC_PDF  -o "$OUT/$pdf"
+	#$PANDOC_PDF "$f"  -o "$OUT/$pdf"
 
 done
 
-cp -r assets/ $OUT
+cp -r templates/assets/ $OUT
 
 
