da259466980aa875659911f07543b27b6d071da3
Jonic Linley <jonic@100yen.co.uk>
2014-04-08 00:13:25 +0100

    Added front-end build tool and dependency management

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    css: {
+      any: 'assets/styles/**/*.scss',
+      input: 'assets/styles/master.scss',
+      output: 'templates/styles/master.min.css'
+    },
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    tag: {
+      banner: '/*!\n' +
+        ' * <%= pkg.name %>\n' +
+        ' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n' +
+        ' * @version <%= pkg.version %>\n' +
+        ' */\n'
+    },
+
+    sass: {
+      options: {
+        banner: '<%= tag.banner %>',
+        require: 'sass-globbing'
+      },
+      dev: {
+        files: {
+          '<%= css.output %>': '<%= css.input %>'
+        },
+        options: {
+          style: 'expanded'
+        }
+      },
+      dist: {
+        files: {
+          '<%= css.output %>': '<%= css.input %>'
+        },
+        options: {
+          style: 'compressed'
+        }
+      }
+    },
+
+    shell: {
+      makeCSS: {
+        options: {
+          stdout: true
+        },
+        command: 'make css'
+      }
+    },
+
+    watch: {
+      css: {
+        files: '<%= css.any %>',
+        tasks: [
+          'sass:dev',
+          'shell:makeCSS'
+        ]
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', [
+    'sass:dev',
+    'shell:makeCSS',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'sass:dist',
+    'shell:makeCSS'
+  ]);
+};
