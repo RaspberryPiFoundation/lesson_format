2da98b8bd97367f86158b641b45500af11651ac7
Andy Lulham <a.lulham@gmail.com>
2014-09-10 20:01:01 +0100

    Add a custom PDF footer with phantomjs

--- rasterize.js
+++ rasterize.js
@@ -1,6 +1,20 @@
 var page = require('webpage').create(),
     system = require('system'),
-    address, output, size;
+    address, output, size,
+    footerContents;
+
+// styled-up footer contents
+footerContents = '\
+<div style="text-align: center; font-family: Helvetica, Arial, sans-serif; font-size: 9px; line-height: 11px;"> \
+    <span style="display: inline-block; border: 2px #00A5DF solid; border-radius: 50%; width: 28px; height: 28px; line-height: 28px; font-size: 22px; color: #00A5DF; background-color: #fff;"> \
+        {{ pageNum }} \
+    </span> \
+    <hr style="height: 4px; border: 0; background-color: #AFE3EF; position: relative; top: -18px; z-index: -999; margin: 0 60px;"> \
+    \
+    These projects are for use inside the UK only. All Code Clubs must be registered. You can check registered clubs on the map at <strong>www.codeclub.org.uk</strong>.<br> \
+    \
+    This coursework is developed in the open on GitHub, at <strong>www.github.com/CodeClub/</strong>. Come and join us! \
+</div>';
 
 if (system.args.length < 3 || system.args.length > 5) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
@@ -13,7 +27,17 @@ if (system.args.length < 3 || system.args.length > 5) {
     if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
         size = system.args[3].split('*');
         page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+        : {
+            format: system.args[3],
+            orientation: 'portrait',
+            margin: {top: '1.2cm'},
+            footer: {
+                height: "2.5cm",
+                contents: phantom.callback(function(pageNum) {
+                    return footerContents.replace('{{ pageNum }}', pageNum);
+                })
+            }
+        };
     }
     if (system.args.length > 4) {
         page.zoomFactor = system.args[4];
