74d68ab43b2e8a1f9a9e037d2cb6b7cb71f3bcaf
Andy Lulham <a.lulham@gmail.com>
2014-09-16 00:12:00 +0100

    Couple of pdf.js fixes
     - Wait for the DOM to fully load before running
     - Split level with regex rather than on word ‘level’. Regexing for a
       number is a more general solution.

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -5,36 +5,41 @@ This file is used by phantomjs to do some bits of html injection
 and reshuffling, to prepare lessons for pdf generation.
 */
 
-/*
-Add a span around the lesson number, so it can be made giant
-in the header.
+window.onload = function () {
+    /*
+    Add a span around the lesson number, so it can be made giant
+    in the header.
 
-NB the alternative is to fix the manifest information and remove the
-word "level" from all, then modify the template accordingly.
-*/
-var lvl = document.getElementsByClassName("level-indicator");
-if (lvl.length > 0 && lvl[0].innerHTML.toLowerCase().indexOf('level ') === 0) {
-    lvl[0].innerHTML = 'Level <span class="level-number">' + lvl[0].innerHTML.substr(6) + '</span>';
-}
-
-/*
-Add some colour to the "Step #:" bit of each activity heading
-*/
-var activities = document.getElementsByClassName("activity");
-for (var x = 0; x < activities.length; x++) {
-    var tag = activities[x].getElementsByTagName('h1');
-    if (tag.length > 0) {
-        var els = tag[0].innerHTML.split(':');
-        if (els.length > 1) {
-            tag[0].innerHTML = '<span class="step-num">' + els.shift() + ':</span>' + els.join(':');
+    NB the alternative is to fix the manifest information and remove the
+    word "level" from all, then modify the template accordingly.
+    */
+    var lvl = document.getElementsByClassName("level-indicator");
+    if (lvl.length > 0) {
+        var lvlParts = lvl[0].innerHTML.match(/(.*?)(\d+)/);
+        if (lvlParts !== null) {
+            lvl[0].innerHTML = lvlParts[1] + '<span class="level-number">' + lvlParts[2] + '</span>';
         }
     }
-}
 
-/* Move the legend to the right place (the end of the first page) */
-var legend = document.getElementById("legend");
-var intro = document.getElementsByClassName("intro");
-if (intro.length > 0) {
-    intro[0].appendChild(legend);
-    legend.style.display = 'block';
+    /*
+    Add some colour to the "Step #:" bit of each activity heading
+    */
+    var activities = document.getElementsByClassName("activity");
+    for (var x = 0; x < activities.length; x++) {
+        var tag = activities[x].getElementsByTagName('h1');
+        if (tag.length > 0) {
+            var els = tag[0].innerHTML.split(':');
+            if (els.length > 1) {
+                tag[0].innerHTML = '<span class="step-num">' + els.shift() + ':</span>' + els.join(':');
+            }
+        }
+    }
+
+    /* Move the legend to the right place (the end of the first page) */
+    var legend = document.getElementById("legend");
+    var intro = document.getElementsByClassName("intro");
+    if (intro.length > 0) {
+        intro[0].appendChild(legend);
+        legend.style.display = 'block';
+    }
 }
