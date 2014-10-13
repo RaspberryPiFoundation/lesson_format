49089073e9e3050ac77958a8924a554a460afac3
Andy Lulham <a.lulham@gmail.com>
2014-10-13 08:58:31 +0100

    One more change to the webhook URL
    It might be useful to just build uk or world… For instance, if we start
    by just launching world only

--- api.py
+++ api.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 q = Queue(connection=conn)
 
-@app.route('/rebuild', methods=['POST'])
-def run():
+@app.route('/rebuild/<region>', methods=['POST'])
+def run(region):
+    regions = ['uk', 'world'] if region == 'all' else region
     repo = request.get_json()["repository"]["name"]
-    for region in ['uk', 'world']:
+    for region in regions:
         # TODO: split the task up so we don't need to use this timeout hack
         q.enqueue_call(func=autobuild, args=(region, repo), timeout=1000)
     return 'Rebuild triggered.'
