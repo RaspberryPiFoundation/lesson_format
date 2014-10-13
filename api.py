7f0d60633ddfc1125cfcf2c221b3af6d24567672
Andy Lulham <a.lulham@gmail.com>
2014-10-13 13:47:13 +0100

    webhook bug: string should be a list.
    If we’re just rebuilding one region, we want to loop through a list of
    one item - not loop through a list of all the constituent letters of
    that one item!

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ q = Queue(connection=conn)
 
 @app.route('/rebuild/<region>', methods=['POST'])
 def run(region):
-    regions = ['uk', 'world'] if region == 'all' else region
+    regions = ['uk', 'world'] if region == 'all' else [region]
     repo = request.get_json()["repository"]["name"]
     for region in regions:
         # TODO: split the task up so we don't need to use this timeout hack
