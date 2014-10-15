e5f649a2aba614a4eb96c1b97bc65eb30497d1d4
Andy Lulham <a.lulham@gmail.com>
2014-10-15 10:36:25 +0100

    Update call to autobuild (since args have changed)

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def run(region):
     repo = request.get_json()["repository"]["name"]
     for region in regions:
         # TODO: split the task up so we don't need to use this timeout hack
-        q.enqueue_call(func=autobuild, args=(region, repo, False), timeout=1000)
+        q.enqueue_call(func=autobuild, args=(region, repo), timeout=1000)
     return 'Rebuild triggered.'
 
 if __name__ == "__main__":
