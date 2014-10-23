e3cfa7878ff380b372ca66f07ec3d042d5b5c8b9
Jonic Linley <jonic@100yen.co.uk>
2014-10-23 15:52:41 +0100

    Re-introduced automatic CSS generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,11 +48,28 @@ module.exports = function (grunt) {
       }
     },
 
+    shell: {
+      css_uk: {
+        options: {
+          stdout: true
+        },
+        command: 'make css_uk'
+      },
+      css_world: {
+        options: {
+          stdout: true
+        },
+        command: 'make css_world'
+      }
+    },
+
     watch: {
       css: {
         files: '<%= css.any %>',
         tasks: [
-          'sass:dev'
+          'sass:dev',
+          'shell:css:uk',
+          'shell:css:world'
         ]
       }
     }
@@ -62,10 +79,14 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'sass:dev',
+    'shell:css_uk',
+    'shell:css_world',
     'watch'
   ]);
 
   grunt.registerTask('build', [
-    'sass:dist'
+    'sass:dist',
+    'shell:css_uk',
+    'shell:css_world'
   ]);
 };
